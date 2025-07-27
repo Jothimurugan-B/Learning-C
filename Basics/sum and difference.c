2d5ccadb07cfc4739661477f71e42d41b6446368
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-07-27 10:28:07 +0530

    Create sum and difference.c
    Beginner level hacker rank problem to find sum and difference of int and float values.

---
+++ Basics/sum and difference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+    float n,m;
+    scanf("%d %d",&a,&b);
+    scanf("%f %f",&n,&m);
+    
+    printf("%d %d\n",a+b,a-b);
+    printf("%.1f %.1f\n",n+m,n-m);
+    
+    return 0;
+}
