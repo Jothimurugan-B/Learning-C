a561844fc0f2b98ecb0d722eda10089f7c6a6510
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-09 07:54:32 +0530

    Create program to find particular element is find in array.c

---
+++ Arrays/program to find particular element is find in array.c
@@ -0,0 +1,27 @@
+//C program to get array elements and find given particular number is found in array or not.
+#include<stdio.h>
+int main() 
+{
+    int a[10],i,n,count=0;
+   
+    printf("Enter array elements:\n");
+    for(i = 0; i < 10; i++) 
+    scanf("%d", &a[i]);
+    
+    printf("Enter number to search:");
+    scanf("%d",&n);
+
+    for(i=0;i<10;i++) 
+    {
+        if(a[i]==n) 
+        {
+            count=1;
+            break;
+        }
+    }
+    if(count)
+        printf("%d is present in the array\n",n);
+    else
+        printf("%d is not present in the array\n",n);
+
+}
