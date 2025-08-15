aa7073a635c712c0ec49e63b2ca2fc1eef16577a
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-15 09:25:19 +0530

    Delete element in array.c

---
+++ Arrays/Delete element in array.c
@@ -0,0 +1,27 @@
+//C program to delete an array element based on index value given.
+#include<stdio.h>
+int main()
+{
+	int a[100],n,pos,i;
+
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+
+	printf("Enter array elements:\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	
+
+	printf("Enter position to delete:");
+	scanf("%d",&pos);
+
+	for(i=pos;i<n-1;i++)
+	{
+		a[i]=a[i+1];
+	}
+	n--;
+	
+	printf("Array after deletion:\n");
+	for(i=0;i<n-1;i++) 
+	printf("%d ",a[i]);
+}
