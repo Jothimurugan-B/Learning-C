1928512024c45ce03000d98c674e01cf73b51037
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-15 22:52:26 +0530

    Upper pyramid.c

---
+++ Patterns/Upper pyramid.c
@@ -0,0 +1,20 @@
+//Upper pyramid pattern
+#include <stdio.h>
+int main()
+{
+   int n,i,j,s;
+   printf("enter a number:\n");
+   scanf("%d",&n);
+   
+   for(i=0;i<n;i++)
+   {
+       for(s=0;s<n-i-1;s++)
+       printf(" ");
+       
+       for(j=0;j<i+1;j++)
+       printf("* ");
+       
+       printf("\n");
+   }
+   
+}
