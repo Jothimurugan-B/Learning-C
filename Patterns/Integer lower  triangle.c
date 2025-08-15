58aa8cc65d68864251f5c1c5c60d42e7b679b81b
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-15 22:43:41 +0530

    Integer lower  triangle.c

---
+++ Patterns/Integer lower  triangle.c
@@ -0,0 +1,16 @@
+//Integer left aligned lower triangle pattern
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("enter n value:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+       for(j=i;j<=n;j++)
+       printf("%d ",j);
+       
+       printf("\n");
+    }
+    
+}
