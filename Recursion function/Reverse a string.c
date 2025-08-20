9341ac960666167430c8e57f8eb52e0c33ef5611
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-20 21:19:34 +0530

    Reverse a string.c

---
+++ Recursion function/Reverse a string.c
@@ -0,0 +1,29 @@
+//C program to reverse a string using recursion function
+#include<stdio.h>
+char* reverse(char*,int,int,char);
+int main()
+{
+    int i=0,j;
+    char s[50],temp;
+    printf("Enter the string:");
+    scanf("%[^\n]",s);
+    for(j=0;s[j];j++);
+    reverse(s,i,j-1,temp);
+    printf("%s",s);
+}
+
+char* reverse(char *d,int i,int j,char temp)
+{
+    if(i<j)
+    {
+      temp=d[i];
+      d[i]=d[j];
+      d[j]=temp;
+      i++;
+      j--;
+      return reverse(d,i,j,temp);
+    }
+    else
+    return d;
+   
+}
