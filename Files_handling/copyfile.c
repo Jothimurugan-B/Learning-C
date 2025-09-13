eadf57d3ceaf792603e117dd8af6aa2e101ad5c9
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-09-13 22:47:14 +0530

    copyfile.c

---
+++ Files_handling/copyfile.c
@@ -0,0 +1,32 @@
+// C Program to copy contents from one file to another
+#include<stdio.h>
+int main()
+{
+	FILE *fp,*fd;
+	int ch;
+	char s[30],d[30];
+
+	printf("Enter source file name:");
+	scanf("%s",s);
+	printf("Enter destination file name:");
+	scanf("%s",d);
+
+	fp=fopen(s,"r");
+	if (fp == NULL) 
+	{
+        printf("Source file not found\n");
+        return 0;
+    }
+    
+	fd=fopen(d,"w");
+
+	while((ch=fgetc(fp))!=EOF)
+	{
+		fputc(ch,fd);
+	}
+
+	fclose(fd);
+	fclose(fp);
+	
+	printf("File copied successfully\n");
+}
